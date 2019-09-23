735421a7b0cd75e9683e420d5e8c4c8c320d855b
zhangyu <zhangyu29@asiainfo.com>
2019-09-23 15:40:09 +0800

    pref: 丰富 hook 用例

--- src/components/hooks/index.jsx
+++ src/components/hooks/index.jsx
@@ -1,20 +1,33 @@
-import React, { useState } from 'react';
-import { Button } from 'antd';
+import React, { useState, useEffect } from "react";
+import { Button } from "antd";
 
 function Example() {
-  // 声明一个新的叫做 “count” 的 state 变量
-  const [count, setCount] = useState(0);
+    // 声明一个新的叫做 “count” 的 state 变量
+    const [count, setCount] = useState(0);
 
-  function handleClick(){
-    setCount(count + 1)
-  }
+    function handleClick() {
+        setCount(count + 1);
+    }
 
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <Button type="primary" onClick={handleClick}>点我</Button>
-    </div>
-  );
+    useEffect(() => {
+        console.log("我只在页面挂载时打印");
+        return () => {
+            console.log("我只在页面卸载时打印");
+        };
+    }, []);
+
+    useEffect(() => {
+        console.log("页面更新时打印");
+    }, [count]);
+
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <Button type="primary" onClick={handleClick}>
+                点我
+            </Button>
+        </div>
+    );
 }
 
 export default Example;
