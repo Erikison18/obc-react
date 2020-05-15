68e4db516799b7873d1ee6a820f0cfafed356f62
zhangyu <zy2071@126.com>
2020-05-15 19:18:17 +0800

    style: 格式化自定义配置文件的代码

--- config/config.custom.js
+++ config/config.custom.js
@@ -1,21 +1,25 @@
-const path = require('path');
+const path = require("path");
 // const homepage = require('./paths').servedPath;
 const homepage = require("react-app-rewired").paths.publicUrlOrPath;
 // servedPath 已被并进 publicUrlOrPath 见 👇
 // https://github.com/facebook/create-react-app/pull/7259/commits/426a1573c50a7d777ace97d7d0a20e8165094f5f#diff-9b26877ecf8d15b7987c96e5a17502f6
+
 module.exports = {
     //本地开发设置iconfont cdn 地址 注意每次ui修改iconfont后都会生成最新的cdn地址，记得及时替换。
-    iconFontCDNUrl:'//at.alicdn.com/t/font_639300_3fp1riujcg5.css',
-    //发布时iconfont位置
-    proIconFontDirectory:path.join(homepage,'/iconfont'),
-    //iconfont生成文件名
-    iconfontFileName:'iconfont',
-    //请求前缀
-    fetchPrefix:'',
-    //使用路由缓存，这种方案不能使用路由动画
-    useKeepAlive:false
-}
+    iconFontCDNUrl: "//at.alicdn.com/t/font_639300_3fp1riujcg5.css",
 
+    //发布时iconfont位置
+    proIconFontDirectory: path.join(homepage, "/iconfont"),
+
+    //iconfont生成文件名
+    iconfontFileName: "iconfont",
+
+    //请求前缀
+    fetchPrefix: "",
+
+    //使用路由缓存，这种方案不能使用路由动画
+    useKeepAlive: false,
+};
 
 /*  useKeepAlive
     1.页面缓存实际上是对我们访问过地址对应的components在页面上进行了display:none和block进行切换。
@@ -23,7 +27,6 @@ module.exports = {
     3.如果访问的路由包含通配符（如：xxx/yyy/:id）和查询参数（如：xxx/yyy?id=1）并且通配符和查询参数有所改变，其实对于当前页面是需要更新数据的。
     4.所以现在我们需要对该情况路由下受到通配符和查询参数影响的conponents进行改造，在该模块下通过UNSAFE_componentWillReceiveProps进行处理数据的更新。
 */
-
 
 /*
 待新增功能：
