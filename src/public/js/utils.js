6528e23d6c7d9f85e553a413d8743031502bdfa5
zhangyu <zy2071@126.com>
2020-05-26 18:26:51 +0800

    perf: utils 文件代码格式化，lint-staged 取消 html 文件的检测

--- src/public/js/utils.js
+++ src/public/js/utils.js
@@ -1,43 +1,39 @@
 //格式化html里的代码换行成渲染时的换行和空格
-export function formatTextCodeToview(string){
-
-    string = string.replace(/[\n\r]/g,'<br/>');
+export function formatTextCodeToview(string) {
+    string = string.replace(/[\n\r]/g, "<br/>");
 
     let reduxSpaceLength = string.match(/^\s*/)[0].length;
-    let reduxSpaceReg = new RegExp(`(^\\s{${reduxSpaceLength}})|(\\<br\\/\\>\\s{${reduxSpaceLength}})`,'g');
+    let reduxSpaceReg = new RegExp(`(^\\s{${reduxSpaceLength}})|(\\<br\\/\\>\\s{${reduxSpaceLength}})`, "g");
 
-    string.replace(reduxSpaceReg,'');
+    string.replace(reduxSpaceReg, "");
 
-    return string.replace(/\s/g,'&nbsp;');
-
+    return string.replace(/\s/g, "&nbsp;");
 }
 
 //获取自动前缀
-export function getAutoPathPrefix(){
-
-    if (process.env.NODE_ENV==='production') return document.body.getAttribute('prefix');
+export function getAutoPathPrefix() {
+    if (process.env.NODE_ENV === "production") return document.body.getAttribute("prefix");
     else return process.env.FETCH_PREFIX;
-
 }
 
 //根据当年月份返回月末的年月日
 // @parmas [YM:require] {string} defualt:null 如2019-04
 // #return [YMD] {string} //返回月末的年月日 如2019-04-30
-export function monthLastDate(YM){
-    let d = new Date(...YM.split('-'),0).getDate();
-    d=toDoubleTime(d);
-    return `${YM}-${d}`
+export function monthLastDate(YM) {
+    let d = new Date(...YM.split("-"), 0).getDate();
+    d = toDoubleTime(d);
+    return `${YM}-${d}`;
 }
 
 //根据当年周数返回月末的年月日
 // @parmas [YM:require] {string} defualt:null 如2019-13周
 // #return [YMD] {string} //返回月末的年月日 如2019-03-31
-export function dayLastDate(YD){
-    const oneDayToStamp=24*60*60*1000;
-    let [year,week]=YD.split('-');
-    week = week.replace(/\D/g,'');
-    let deviation = new Date(new Date(year).getTime()-oneDayToStamp).getDay();
-    let stamp = new Date(year+'',0,0).getTime()+week*7*oneDayToStamp-deviation*oneDayToStamp;
+export function dayLastDate(YD) {
+    const oneDayToStamp = 24 * 60 * 60 * 1000;
+    let [year, week] = YD.split("-");
+    week = week.replace(/\D/g, "");
+    let deviation = new Date(new Date(year).getTime() - oneDayToStamp).getDay();
+    let stamp = new Date(year + "", 0, 0).getTime() + week * 7 * oneDayToStamp - deviation * oneDayToStamp;
     let time = new Date(stamp);
     let y = toDoubleTime(time.getFullYear());
     let m = toDoubleTime(time.getMonth() + 1);
@@ -46,6 +42,6 @@ export function dayLastDate(YD){
 }
 
 //时间格式化，返回2位以上的数
-export function toDoubleTime(num){
-    return String(num).length>1?num:`0${num}`
+export function toDoubleTime(num) {
+    return String(num).length > 1 ? num : `0${num}`;
 }
