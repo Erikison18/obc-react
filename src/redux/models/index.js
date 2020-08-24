0561845c2fcb8bc3b43c96fb1f69e88391667920
zhangyu <zy2071@126.com>
2020-08-24 22:22:27 +0800

    refactor：🎨 1.按标准 修正模块导出语句    2.新增 pages 文件夹

--- src/redux/models/index.js
+++ src/redux/models/index.js
@@ -3,7 +3,7 @@
 export后提供给combineReducers组合后导入到整个store中
 */
 export { loadingBarReducer as loadingBar } from "react-redux-loading-bar";
-export asyncDemo from "./test/async.js";
-export count from "./test/count.js";
-export complex from "./test/complex.js";
-export reselector from "./test/reselector.js";
+export { default as asyncDemo } from "./test/async.js";
+export { default as count } from "./test/count.js";
+export { default as complex } from "./test/complex.js";
+export { default as reselector } from "./test/reselector.js";
